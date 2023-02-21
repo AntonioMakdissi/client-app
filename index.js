906e64acbf2b4799c04383bf6ac14dd18afedce7
Antonio Al Makdissi <antoniomakdissi1@gmail.com>
2023-02-21 14:46:16 +0200

    dynamicTest

---
+++ index.js
@@ -0,0 +1,17 @@
+
+fetch("https://apitest-90no.onrender.com/data")
+    .then(response => response.json())
+    .then(json => {
+        json.forEach(student => {
+            var content = `
+            <div class="icon"><i class="bi bi-card-checklist"></i></div>
+            <h4 class="title"><a href="">Dolor Sitema</a></h4>
+            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+          `;
+
+            var div = document.createElement('div')
+            div.innerHTML = content;
+            div.className = 'col-lg-4 col-md-6 icon-box'
+            document.getElementById('ServicesRow').appendChild(div);
+        });
+    });
